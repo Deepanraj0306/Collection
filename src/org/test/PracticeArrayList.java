cf84b6e34e8298b28e4b822b6ac5c64422c59e40
Deepanraj0306 <deepanraj030696@gmail.com>
2022-03-15 20:20:51 +0530

    Folder Structure

---
+++ src/org/test/PracticeArrayList.java
@@ -0,0 +1,74 @@
+package org.test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class PracticeArrayList {
+	public static void main(String[] args) {
+		
+		List<String> l=new ArrayList<>();
+		l.add("BMW");
+		l.add("Benz");
+		l.add("Beneley");
+		l.add("Jaguar");
+		l.add("RollsRoyce");
+		l.add("Honda");
+		l.add("Null");
+		l.add("Null");
+		l.add("BMW");
+		
+		System.out.println(l);
+		int size = l.size();
+		System.out.println(size);
+		
+		System.out.println("====for loop====");
+		
+		for (int i = 0; i < l.size(); i++) {
+			System.out.println(l.get(i));
+		}
+		
+		System.out.println("====Enhanced loop====");
+		
+		for (String string : l) {
+			System.out.println(string);
+		}
+		
+		System.out.println("====Using ListIterator forward====");
+		
+		ListIterator<String> listIterator = l.listIterator();
+		while(listIterator.hasNext()) {
+			System.out.println(listIterator.next());
+		}
+		
+		System.out.println("====Using ListIterator previous====");
+		
+		while(listIterator.hasPrevious()) {
+			System.out.println(listIterator.previous());
+		}
+		
+		System.out.println("====Using Iterator====");
+		
+		Iterator<String> iterator = l.iterator();
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+		System.out.println("====Using Addall====");
+		
+		List<String> li=new ArrayList<>();
+		li.addAll(l);
+		System.out.println(li);
+		
+		l.add(7,"Kia");
+		System.out.println(l);
+		System.out.println(l.get(1));
+		
+		l.set(2,"Volvo");
+		System.out.println(l);
+		
+		
+	
+	}
+	
+}
